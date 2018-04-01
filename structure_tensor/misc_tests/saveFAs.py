90fa2f2e1197689e8fc24a13697b7f0e4c2f3530
Scott Trinkle <tscott.trinkle@gmail.com>
2018-04-01 18:35:23 -0500

    Adds better logging

--- structure_tensor/misc_tests/saveFAs.py
+++ structure_tensor/misc_tests/saveFAs.py
@@ -5,17 +5,21 @@ sys.path.append('..')
 import st_tools as st
 import tifffile as tf
 
-print('Reading')
+print('Reading\n')
 im = io.imread('../../data/recon_2x_stack-1.tif')
 
-print('Running STA')
+print('Running STA\n')
 sl = 35
 dsigs = [0.5, 1, 2, 3, 4, 5, 10, 20]
 nsigs = [0.5, 1, 2, 3, 4, 5, 10, 20]
 
 for dsig, nsig in zip(dsigs, nsigs):
+    print('dsig: {}\nnsig: {}'.format(dsig, nsig))
     FA, vects = st.st_3D(im,
                          d_sigma=dsig,
                          n_sigma=nsig,
                          westin=True)
+    print('Saving...\n')
     tf.imsave('FAs/d{}_n{}.tif'.format(dsig, nsig), im[sl])
+
+print('Done!')
