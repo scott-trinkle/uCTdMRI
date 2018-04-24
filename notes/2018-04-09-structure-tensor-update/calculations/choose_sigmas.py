Implements img_as_float, fixes uint8 overflow error

notes/2018-04-09-structure-tensor-update/calculations/choose_sigmas.py
@@ -7,18 +7,18 @@ print('Reading image')
 im = imread('../../../data/xray/recon_2x_stack-1.tif')
 
 print('Structure tensor analysis')
-d = 2
-for n in [3, 5, 6, 7]:
-    print('n = {}'.format(n))
-    AI, vectors = StructureTensor(im,
-                                  d_sigma=d,
-                                  n_sigma=n).results()
+for d in [1, 2, 3]:
+    for n in [1, 2, 3, 5]:
+        print('n = {}'.format(n))
+        AI, vectors = StructureTensor(im,
+                                      d_sigma=d,
+                                      n_sigma=n).results()
 
-    # To make x = red, y = green, z = blue
-    vectors = np.flip(vectors, axis=-1)
+        # To make x = red, y = green, z = blue
+        vectors = np.flip(vectors, axis=-1)
 
-    print('Saving\n')
-    imsave(fn='choose_sigmas/RGB_d{}_n{}.tif'.format(d, n),
-           im=vectors,
-           rgb=True,
-           scalar=AI)
+        print('Saving\n')
+        imsave(fn='choose_sigmas/RGB_d{}_n{}.tif'.format(d, n),
+               im=vectors,
+               rgb=True,
+               scalar=AI)