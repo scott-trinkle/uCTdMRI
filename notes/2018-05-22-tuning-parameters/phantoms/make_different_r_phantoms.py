cdd65ef96e956a2df4c0c8e6f99fddc1a9658c77
Scott Trinkle <tscott.trinkle@gmail.com>
2018-05-25 16:02:26 -0500

    Changes different r phantoms from z to x

--- notes/2018-05-22-tuning-parameters/phantoms/make_different_r_phantoms.py
+++ notes/2018-05-22-tuning-parameters/phantoms/make_different_r_phantoms.py
@@ -5,10 +5,10 @@ from phantom_funcs import make_orth_phantom
 n_fibers = 9
 for r in [4, 8, 12, 16]:
     print('r = ', r)
-    phant, mask = make_orth_phantom(orient=0,
+    phant, mask = make_orth_phantom(orient=2,
                                     n_fibers=n_fibers,
                                     r=r)
-    imsave('different_size/masks/z_phantom_nfib9_r{}.tif'.format(r),
+    imsave('different_size/masks/x_phantom_nfib9_r{}.tif'.format(r),
            mask, dtype=np.float32)
-    imsave('different_size/phants/z_phantom_nfib9_r{}.tif'.format(r),
+    imsave('different_size/phants/x_phantom_nfib9_r{}.tif'.format(r),
            phant)
