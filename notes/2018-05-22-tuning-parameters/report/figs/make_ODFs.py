5d843d965ac3c821bcdc4d479639bdbdeb658e44
Scott Trinkle <tscott.trinkle@gmail.com>
2018-05-31 16:04:43 -0500

    Fixes camera angle on ODF plots for notes

--- notes/2018-05-22-tuning-parameters/report/figs/make_ODFs.py
+++ notes/2018-05-22-tuning-parameters/report/figs/make_ODFs.py
@@ -12,23 +12,22 @@ sphere = odftools.make_sphere(1500)
 deg45_odf = odftools.get_odf(deg45_c, sphere)
 r8_odf = odftools.get_odf(r8_c, sphere)
 
-interactive = True
+interactive = False
 save = False
 
 ren45 = window.Renderer()
 ren45 = show_ODF(deg45_odf, sphere, interactive=interactive, ren=ren45,
                  save=save, fn='deg45_ODF.png')
 
-camera = ren45.GetActiveCamera()
-camera.SetUseExplicitProjectionTransformMatrix(False)
-# camera.SetViewAngle(10.)
-# camera.SetPosition(10, 10, 10)
-# ren45.SetActiveCamera(camera)
-# window.show(ren45)
-
+position, fp, up = ren45.get_camera()
+scale = 5
+position = scale * np.ones(3)
+ren45.set_camera(position, fp, up)
 
 ren8 = window.Renderer()
 ren8 = show_ODF(r8_odf, sphere, interactive=interactive,
                 ren=ren8, save=save, fn='r8_ODF.png')
-ren8.SetActiveCamera(camera)
-window.show(ren8)
+ren8.set_camera(position, fp, up)
+
+window.record(ren45, out_path='deg45_ODF.png', reset_camera=False)
+window.record(ren8, out_path='r8_ODF.png', reset_camera=False)
