2c05fa5a14b85434689a58ead368d978eca1fbe2
Scott Trinkle <tscott.trinkle@gmail.com>
2018-09-26 14:54:34 -0500

    Generalizes to fn variable

--- notes/2018-08-22-bit-depth/check_slices.py
+++ notes/2018-08-22-bit-depth/check_slices.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from glob import glob
 
-fns = glob('../../../Data/2017_07_22_WholeMouseMRI_5x_2k_phase35cm_gap31_exp30_newfocus/recon_flatcorr_1x/recon_crop_8/*')
+path = '../../../Data/2018_04_03_WholeBrainMRI1_retake_newfocus/recon_flatcorr_1x/recon_crop8_new/*.tiff'
+fns = glob(path)
 nums = np.sort([int(fn.split('.tiff')[0].split('_')[-1]) for fn in fns])
+diff = np.setdiff1d(range(0, 13310), nums)
 
-diff = np.setdiff1d(range(0, 13782), nums)
-
-fig = plt.figure(figsize=(20, 6))
-plt.plot(diff[:100], np.diff(diff)[:100], 'k.:')
-plt.xlabel('Slice number')
-plt.ylabel('Distance to next missing slice')
-plt.title('Missing slices')
-plt.tight_layout()
-plt.show()
+plot = False
+if plot:
+    fig = plt.figure(figsize=(20, 6))
+    plt.plot(diff[:100], np.diff(diff)[:100], 'k.:')
+    plt.xlabel('Slice number')
+    plt.ylabel('Distance to next missing slice')
+    plt.title('Missing slices')
+    plt.tight_layout()
+    plt.show()
